b2e1035d166224e84d2cf5d959ffc2ac8e84aa54
Abdallah Samy <abdallhsamy2011@gmail.com>
2020-11-11 10:08:50 +0200

    update method

--- src/RouterBuilder.php
+++ src/RouterBuilder.php
@@ -3,88 +3,149 @@
 namespace Abdallhsamy\RouterBuilder;
 use Illuminate\Routing\Router as IlluminateRouter;
 
-class RouterBuilder
+class RouterBuilder extends IlluminateRouter
 {
+//    private $controller;
+//    private $prefix1;
+//    private array $methods;
+//    private $middleware1;
+//    private bool $resourceRouter = false;
+//    private bool $apiRsourceRouter = false;
+//    private array $routes;
+//    private $router;
+//
+//    /**
+//     * RouterBuilder constructor.
+//     * @param IlluminateRouter $illuminateRouter
+//     */
+//    public function __construct(IlluminateRouter $illuminateRouter)
+//    {
+//        $this->router = $illuminateRouter;
+//
+//    }
+//
+//    /**
+//     * Set Controller
+//     * @param $controller
+//     * @return RouterBuilder
+//     */
+//    public function class($controller): RouterBuilder
+//    {
+//        $this->controller = $controller;
+//        return $this;
+//    }
+//
+//    /**
+//     * Set Prefix
+//     * @param $prefix
+//     * @return RouterBuilder
+//     */
+//    public function prefix($prefix): RouterBuilder
+//    {
+//        $this->prefix1 = $prefix;
+//        return $this;
+//    }
+//
+//    /**
+//     * Set Middleware
+//     * @param $middleware
+//     * @return RouterBuilder
+//     */
+//    public function middleware($middleware): RouterBuilder
+//    {
+//        $this->middleware1 = $middleware;
+//        return $this;
+//    }
+//
+//    /**
+//     * Set RouterBuilder as resource router
+//     * @return RouterBuilder
+//     */
+//    public function resource(): RouterBuilder
+//    {
+//        $this->resourceRouter = true;
+//        return $this;
+//    }
+//
+//    /**
+//     * Set RouterBuilder as apiResource router
+//     * @return RouterBuilder
+//     */
+//    public function apiResource(): RouterBuilder
+//    {
+//        $this->apiRsourceRouter = true;
+//        return $this;
+//    }
+//
+//    /**
+//     * add router to the group
+//     * @return RouterBuilder
+//     */
+//    public function newRouter($method, $uri, $action, $name): RouterBuilder
+//    {
+//        $route = $this->router->$method();
+//        $this->routes[] = $route;
+//        return $this;
+//    }
+
     private $controller;
-    private $prefix1;
-    private array $methods;
-    private $middleware1;
-    private bool $resourceRouter = false;
-    private bool $apiRsourceRouter = false;
-    private array $routes;
-    private $router;
+    private $prefix;
+    private bool $grouped = false;
+    private array $siblings;
 
-    /**
-     * RouterBuilder constructor.
-     */
-    public function __construct(IlluminateRouter $illuminateRouter)
-    {
-        $this->router = $illuminateRouter;
 
-    }
-
-    /**
-     * Set Controller
-     * @param $controller
-     * @return RouterBuilder
-     */
-    public function class($controller): RouterBuilder
+    public function setController($controller): RouterBuilder
     {
         $this->controller = $controller;
         return $this;
     }
 
-    /**
-     * Set Prefix
-     * @param $prefix
-     * @return RouterBuilder
-     */
-    public function prefix($prefix): RouterBuilder
+    public function setPrefix($prefix)
     {
-        $this->prefix1 = $prefix;
+        $this->prefix = $prefix;
         return $this;
     }
 
-    /**
-     * Set Middleware
-     * @param $middleware
-     * @return RouterBuilder
-     */
-    public function middleware($middleware): RouterBuilder
+    public function grouped(): RouterBuilder
     {
-        $this->middleware1 = $middleware;
+        $this->grouped = true;
         return $this;
     }
 
-    /**
-     * Set RouterBuilder as resource router
-     * @return RouterBuilder
-     */
-    public function resource(): RouterBuilder
-    {
-        $this->resourceRouter = true;
-        return $this;
-    }
-
-    /**
-     * Set RouterBuilder as apiResource router
-     * @return RouterBuilder
-     */
-    public function apiResource(): RouterBuilder
-    {
-        $this->apiRsourceRouter = true;
-        return $this;
-    }
 
     /**
      * add router to the group
+     * @param string $method
+     * @param string $uri
+     * @param string $action
+     * @param string|null $name
      * @return RouterBuilder
      */
-    public function newRouter($method, $uri, $action, $name): RouterBuilder
+    public function newRouter(
+        string $method = 'get',
+        string $uri = '/',
+        string $action = '__invoke',
+        string $name = null
+    ): RouterBuilder
     {
-        $route = $this->router->$method();
-        $this->routes[] = $route;
+        $route = $this->$method($uri, [$this->controller, $action]);
+
+        if ($name) {
+            $route->name($name);
+        }
+
+        $this->siblings[] = $route;
         return $this;
     }
 
+    public function generate()
+    {
+        $this->prefix($this->prefix)->group(function (RouterBuilder $router) {
+            $this->siblings;
+            foreach ($this->siblings as $route) {
+                $this->$route->method($route->uri, [$this->controller, $route->action]);
+            }
+        });
+        return $this;
+    }
 }
